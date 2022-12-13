18b07de619faf047bb7756a091612d0e3eee0708
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2022-12-13 11:03:25 +0100

    settings update

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ POSTGRES_PASS = str(os.getenv('POSTGRES_PASSWORD') or 'password')
 
 STRUCTURED_DATA_TOPIC = str(os.getenv('STRUCTURED_DATA_TOPIC') or 'STRUCTURED_DATA_TOPIC')
 
-STRUCTURED_DATA_SCHEMA_PATH = str(os.getenv('STRUCTURED_DATA_SCHEMA_PATH') or './avro/structured_data.avsc')
+STRUCTURED_DATA_SCHEMA_PATH = str(os.getenv('STRUCTURED_DATA_SCHEMA_PATH') or './avro_files/structured_data.avsc')
 
 BOOTSTRAP_SERVERS = str(os.getenv('BOOTSTRAP_SERVERS') or 'localhost:9092')
 SCHEMA_REGISTRY_URL = str(os.getenv('SCHEMA_REGISTRY_URL') or 'http://localhost:8085')
